056267ed9a4e98e7ad95d5ac8f456ece1bbd3599
Tran Van Thao <beltatu01@gmail.com>
2026-05-10 00:05:30 +0700

    fok1

--- src/main/java/com/forumapp/ForumAppApplication.java
+++ src/main/java/com/forumapp/ForumAppApplication.java
@@ -2,10 +2,12 @@ package com.forumapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories(basePackages = "com.forumapp.repository")
 public class ForumAppApplication {
 
     public static void main(String[] args) {
